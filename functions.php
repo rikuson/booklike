83f69ae8235647d36c9567be7a0a5bb017333433
Rikson <noskir123@yahoo.co.jp>
2017-08-12 09:12:28 +0800

    Add template

---
+++ functions.php
@@ -0,0 +1,114 @@
+<?php
+/*
+ * project   : OD Base
+ * file name : functions.php
+ * created   : 2017/06/13
+ */
+
+if ( ! function_exists( 'odbase_setup' ) ) :
+
+function odbase_setup() {
+	/*
+	 * 自動フィードリンク
+	 */
+	add_theme_support( 'automatic-feed-links' );
+	
+	/*
+	 * titleを自動で書き出し
+	 */
+	add_theme_support( 'title-tag' );
+	
+	/*
+	 * アイキャッチ画像を設定できるようにする
+	 */
+	add_theme_support( 'post-thumbnails' );
+	
+	/*
+	 * 検索フォーム、コメントフォーム、コメントリスト、ギャラリー、キャプションでHTML5マークアップの使用を許可します
+	 */
+	add_theme_support( 'html5', array(
+		'search-form',
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'caption',
+	) );
+	
+	/*
+	 * カスタム背景
+	 */
+	$args = array(
+		'default-color' => 'ffffff',
+		'default-image' => '',
+	);
+	add_theme_support( 'custom-background', $args );
+	
+	/*
+	 * テーマカスタマイザーにおける編集ショートカット機能の使用
+	 */
+	add_theme_support( 'customize-selective-refresh-widgets' );
+	
+	/*
+	 * カスタムメニュー位置を定義
+	 */
+	register_nav_menus( array(
+		'global' => 'グローバルナビ',
+	) );
+	
+}
+endif;
+add_action( 'after_setup_theme', 'odbase_setup' );
+
+/*
+ * 動画や写真を投稿する際のコンテンツの最大幅を設定
+ */
+function odbase_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'odbase_content_width', 640 );
+}
+add_action( 'after_setup_theme', 'odbase_content_width', 0 );
+
+/*
+ * サイドバーを定義
+ */
+function odbase_widgets_init() {
+	register_sidebar( array(
+		'name'          => 'Sidebar',
+		'id'            => 'sidebar',
+		'description'   => 'ここにウィジェットを追加',
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'odbase_widgets_init' );
+
+/*
+ * スクリプトを読み込み
+ */
+function odbase_scripts() {
+	wp_enqueue_style( 'odbase-style', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'odbase_scripts' );
+
+/*
+ * カスタヘッダーの設定
+ */
+$args = array(
+	'width'         => 1800,
+	'height'        => 600,
+	'default-image' => '',
+);
+add_theme_support( 'custom-header', $args );
+
+
+/*
+ * read more ボタンをexcerpt出力の後に設置
+ */
+function new_excerpt_more( $more ) {
+	return '<a class="read-more" href="'. get_permalink( get_the_ID() ) . '">もっと読む</a>';
+}
+add_filter( 'excerpt_more', 'new_excerpt_more' );
+
+
+?>
---
+++ single.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * project   : OD Base
+ * file name : single.php
+ * created   : 2017/06/16
+ */
+?>
+<?php get_header(); ?>
+	<main id="primary" class="content-area">
+		<?php while ( have_posts() ): the_post(); ?>
+		<?php
+			// カテゴリー名をリンクなしで取得したい場合
+			$cat = get_the_category();
+			$cat = $cat[0];
+			// 出力はline.23
+		?>
+		<article id="post-<?php the_id(); ?>" <?php post_class(); ?>>
+			<header class="entry-header">
+				<h1 class="entry-title"><?php the_title(); ?></a></h1>
+				<div class="entry-meta">
+					<span class="date">投稿日 : <?php the_time( get_option( 'date_format' ) ); ?></span>
+					<span class="category">カテゴリー : <?php if ( $cat ) { echo esc_html( $cat->name ); } // カテゴリー名（リンクなし）を表示 ?></span>
+					<span class="categori-links">カテゴリー : <?php the_category( '、' ); ?></span>
+					<span class="tag-links">タグ : <?php the_tags( '', '、' ); ?></span>
+					<span class="author">投稿者 : <?php the_author(); ?></span>
+				</div>
+				<?php if ( has_post_thumbnail() ): ?>
+				<figure class="entry-thumbnail">
+					<?php the_post_thumbnail(); ?>
+				</figure>
+				<?php 
+					/* else:
+					 * サムネイルがない場合に挿入する画像を指定
+					 * その際にはfigureの位置を再考せよ
+					*/
+				?>
+				<?php endif; ?>
+			</header>
+			<div class="entry-content">
+				<?php the_content(); ?>
+			</div>
+			<!--
+			#
+			# 記事最下部にメタ情報を表示したい場合に利用
+			# スタイリングは記事ヘッダー部分のメタ情報と同じ
+			# その他、アドセンスやバナー、関連記事などはこちらに入れる方が良いかも
+			#
+			<footer class="entry-footer">
+				<div class="entry-meta">
+					<span class="date">投稿日 : <?php the_time( get_option( 'date_format' ) ); ?></span>
+					<span class="category">カテゴリー : <?php if ( $cat ) { echo esc_html( $cat->name ); } // カテゴリー名（リンクなし）を表示 ?></span>
+					<span class="categori-links">カテゴリー : <?php the_category( '、' ); ?></span>
+					<span class="tag-links">タグ : <?php the_tags( '', '、' ); ?></span>
+					<span class="author">投稿者 : <?php the_author(); ?></span>
+				</div>
+			</footer>
+			-->
+		</article>
+		<?php
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+			
+			endwhile;
+		
+			the_post_navigation();
+		?>
+	</main>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
