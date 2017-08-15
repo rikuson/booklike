9df52f140417cc0f0fc062cf8a009bd4be4c7a78
Rikson <noskir123@yahoo.co.jp>
2017-08-15 10:27:48 +0800

    Update index
    Enable to show all updates.

--- index.php
+++ index.php
@@ -3,31 +3,51 @@
 ?>
 <main>
 	<?php
-		$categories = get_categories();
-		foreach ($categories as $category) :
+		// カテゴリをキーにして並び替える
+		$posts_order_by_category = array();
+		while (have_posts()) {
+			the_post();
+			$formated_post = array(
+				'link' => get_the_permalink(),
+				'title' => get_the_title(),
+				'status' => '作成',
+				'date' => get_the_time(get_option('date_format')),
+				'timestamp' => get_the_time('U')
+			);
+			$categories = get_the_category();
+			foreach ($categories as $category) {
+				$posts_order_by_category[$category->term_id][] = $formated_post;
+				// 記事更新の場合は記事作成とは別にパースする
+				if (get_the_time('U') !== get_the_modified_time('U')) {
+					$formated_post['status'] = '更新';
+					$formated_post['date'] = get_the_modified_date();
+					$formated_post['timestamp'] = get_the_modified_time('U');
+					$posts_order_by_category[$category->term_id][] = $formated_post;
+				}
+			}
+		}
+		wp_reset_query();
+
+		function sortByTimestamp($a, $b){
+			return $b['timestamp'] - $a['timestamp'];
+		}
+
+		foreach ($posts_order_by_category as $cat_ID => $posts) :
 	?>
-	<h2><?php echo $category->name; ?></h2>
+	<h2>
+		<?php
+			$cat_name = get_the_category_by_ID($cat_ID);
+			$cat_link = get_category_link($cat_ID);
+			echo '<a href="' . $cat_link . '">' . $cat_name . '</a>';
+		?>
+	</h2>
 	<ul>
 		<?php
-			$query = array(
-				'cat' => $category->term_id,
-				'orderby' => 'modified'
-			);
-			query_posts($query);
-			while (have_posts()) :
-			the_post();
-			$status = '';
-			$date = '';
-			if (get_the_time('U') === get_the_modified_time('U')) {
-				$status = '作成';
-				$date = get_the_time(get_option('date_format'));
-			} else {
-				$status = '更新';
-				$date = get_the_modified_date();
-			}
+			usort($posts, 'sortByTimestamp');
+			foreach ($posts as $post) :
 		?>
-		<li><?php echo $date; ?> - <?php echo $status; ?> - <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></li>
-		<?php endwhile; wp_reset_query(); ?>
+		<li><?php echo $post['date']; ?> - <?php echo $post['status']; ?> - <a href="<?php echo $post['link']; ?>" rel="bookmark"><?php echo $post['title']; ?></a></li>
+		<?php endforeach; ?>
 	</ul>
 	<?php endforeach; ?>
 </main>
