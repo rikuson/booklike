bb8bf37a0123656098a62c4a8fa4a496025cb59c
Rikson <noskir123@yahoo.co.jp>
2017-08-17 17:30:48 +0800

    Move entry meta to header from footer

--- single.php
+++ single.php
@@ -1,8 +1,13 @@
 <?php get_header(); ?>
-<main id="primary" class="content-area">
+<main class="content-area">
 	<?php while ( have_posts() ): the_post(); ?>
 	<article id="post-<?php the_id(); ?>" <?php post_class(); ?>>
 		<header class="entry-header">
+			<div class="entry-meta">
+				<span class="created-at">作成 : <time><?php the_time( get_option( 'date_format' ) ); ?></time></span>
+				<span class="updated-at">更新 : <time><?php the_modified_time( get_option( 'date_format' ) ); ?></time></span>
+				<span class="author">投稿者 : <?php the_author(); ?></span>
+			</div>
 			<?php if ( has_post_thumbnail() ): ?>
 			<figure class="entry-thumbnail">
 				<?php the_post_thumbnail('full'); ?>
@@ -13,11 +18,6 @@
 			<?php the_content(); ?>
 		</div>
 		<footer class="entry-footer">
-			<div class="entry-meta">
-				<span class="created-at">作成 : <time><?php the_time( get_option( 'date_format' ) ); ?></time></span>
-				<span class="updated-at">更新 : <time><?php the_modified_time( get_option( 'date_format' ) ); ?></time></span>
-				<span class="author">投稿者 : <?php the_author(); ?></span>
-			</div>
 			<?php
 				the_post_navigation(array(
 					'prev_text' => '&larr;&nbsp;%title',
